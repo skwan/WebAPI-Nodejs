923ed53d116fa4bb4b0d076f2205110bd3246d72
skwan <skwan@microsoft.com>
2015-07-11 12:14:25 -0700

    Initial commit

--- node-server/config.js
+++ node-server/config.js
@@ -1,6 +1,6 @@
  // Don't commit this file to your public repos. This config is for first-run
     exports.creds = {
     mongoose_auth_local: 'mongodb://localhost/tasklist', // Your mongo auth uri goes here
-    audience: 'http://localhost:8888/', // the Audience is the App URL when you registered the application.
+    audience: 'https://skwantoso.com/webapp-nodejs', // the Audience is the App URL when you registered the application.
     identityMetadata: 'https://login.microsoftonline.com/common/' // Replace the text after p= with your specific policy.
   };
